374d36820fcf954bde980779836afe1d3b36a53e
Felipe Gama <felipegamae2@gmail.com>
2025-10-02 20:10:19 -0300

    Commit 001
    - Adicionando arquivos do projeto

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-#Definir o que precisa ser feito neste código
+#Menu principal content
+
+from src.login_page import login
+# from src.register_page import ...
+
+def main_menu():
+    print('===== Welcome to the SGB =====')
+    print('< 1 > Login')
+    print('< 2 > Cadastro')
+    opc = int(input('-> [ ]\b\b'))
+    return opc
+
+def switch_case(opc):
+    match opc:
+        case 1:
+            #esperando a página de login ser feita
+            print('Esperando a tela de login ser feita...')
+            login()
+        case 2:
+            #esperando a tela de ergistros ser feita
+            print('Esperando a tela de Registros ser feita...')
+        case _:
+            main_menu()
+            opc = 0
+    
+
+switch_case(main_menu())

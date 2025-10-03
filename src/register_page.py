311e6080e3c39d50196e3b68061ba7e0ecd948b3
Felipe Gama <felipegamae2@gmail.com>
2025-10-03 16:21:52 -0300

    Commit 3
    Apagando content da register_page.py

--- src/register_page.py
+++ src/register_page.py
@@ -1,18 +1,1 @@
-def register():
-    print('Who are you?')
-    print('< 1 > The administrator')
-    print('< 2 > A Student')
-    print('< 3 > A teacher')
-    option = int(input('-> '))
-
-    if option == 1:
-        name_admin = input('Your name: ')
-        email_admin = input('Your best email: ')
-        cpf_admin = input('CPF: ') #Put a regex here to take the cpf from admin
-
-        admin = {
-            "_name": name_admin,
-            "email": email_admin,
-            "cpf":  
-        }
 

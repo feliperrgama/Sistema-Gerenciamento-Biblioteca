a03ec7fa962a75a1a957d8534ed929b1ca9d3ee1
Felipe Gama <felipegamae2@gmail.com>
2025-10-05 22:02:22 -0300

    Commit 006
    - Melhorando o main menu do sistema no arquivo main.py

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 
 from src.login_page import login
 # from src.register_page import ...
+from src.loading_animation import loadingAnimation
+from os import system
 
 def main_menu():
+    system('clear')
     print('===== Welcome to the SGB =====')
     print('< 1 > Login')
     print('< 2 > Cadastro')
@@ -15,13 +18,21 @@ def switch_case(opc):
     match opc:
         case 1:
             #esperando a página de login ser feita
+            system('clear')
+            print('Message: Indo para a tela de login...')
+            loadingAnimation()
             print('Esperando a tela de login ser feita...')
             login()
         case 2:
             #esperando a tela de ergistros ser feita
+            system('clear')
+            print('Message: Indo para a tela de cadastro...')
+            loadingAnimation()
             print('Esperando a tela de Registros ser feita...')
         case _:
-            print('Opção inválida!\nTente outra vez...')
+            system('clear')
+            print('Message: Opção inválida! Tente outra vez...')
+            loadingAnimation()
             main_menu()
             opc = 0
     
